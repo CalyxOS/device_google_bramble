2123fddb395bafbb059cd5abf5bc07ce2c971935
Chirayu Desai <chirayudesai1@gmail.com>
2021-03-25 22:08:57 +0530

    Include CarrierConfig overlay from vendor
    Change-Id: I489a9b8d8b8f13ca79d389b3b2280a44559a7799

--- calyx_bramble.mk
+++ calyx_bramble.mk
@@ -5,6 +5,8 @@ $(call inherit-product, vendor/calyx/config/common.mk)
 $(call inherit-product, device/google/redbull/device-calyx.mk)
 $(call inherit-product, device/google/bramble/aosp_bramble.mk)
 
+DEVICE_PACKAGE_OVERLAYS += vendor/google_devices/bramble/vendor_overlay/carrier-naked/
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := calyx_bramble
 PRODUCT_MODEL := Pixel 4a (5G)
