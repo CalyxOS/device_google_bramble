cf6852ee48e77a80a2ddb542e01cdcd4f0a8c50a
Michael Bestas <mkbestas@gmail.com>
2024-10-17 18:57:23 +0300

    Switch to new property override scheme
    Change-Id: I0c787e99c52eccf785a68da62b4aaeba37093a3e

--- calyx_bramble.mk
+++ calyx_bramble.mk
@@ -11,11 +11,10 @@ PRODUCT_NAME := calyx_bramble
 PRODUCT_MODEL := Pixel 4a (5G)
 PRODUCT_BRAND := google
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_PRODUCT=bramble \
-    PRIVATE_BUILD_DESC="bramble-user 14 UP1A.231105.001.B2 11260668 release-keys"
-
-BUILD_FINGERPRINT := google/bramble/bramble:14/UP1A.231105.001.B2/11260668:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES := \
+    BuildDesc=bramble-user[[:space:]]15[[:space:]]UP1A.231105.001.B2[[:space:]]11260668[[:space:]]release-keys \
+    BuildFingerprint=google/bramble/bramble:15/UP1A.231105.001.B2/11260668:user/release-keys \
+    DeviceProduct=$(DEVICE_CODENAME)
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
